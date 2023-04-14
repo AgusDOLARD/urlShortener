a5dd7f146be45eb88c6eb73c11a16e8e007d9f0b
AgusDOLARD <agust_22_10@hotmail.com>
2023-04-14 01:00:14 -0300

    parsed the url instead of using MESSI regex

--- controllers/urlControllers.go
+++ controllers/urlControllers.go
@@ -1,52 +1,52 @@
 package controllers
 
 import (
-	"regexp"
-	"urlShortener/initializers"
-	"urlShortener/models"
+	"net/url"
 
+	"github.com/AgusDOLARD/urlShortener/initializers"
+	"github.com/AgusDOLARD/urlShortener/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/twharmon/gouid"
 )
 
 // URLSolve GET "/:slug" lookup the slug in the database and return the full url
 func URLSolve(c *fiber.Ctx) error {
-	var url models.URL
+	var u models.URL
 
 	slug := c.Params("slug")
 	if slug == "" {
 		return fiber.NewError(fiber.StatusBadRequest, "slug not provided")
 	}
 
-	initializers.DB.First(&url, "slug = ?", slug)
-	if url.ID == 0 {
+	initializers.DB.First(&u, "slug = ?", slug)
+	if u.ID == 0 {
 		return fiber.NewError(fiber.StatusBadRequest, "failed to find the slug")
 	}
 
-	return c.Redirect(url.Full)
+	return c.Redirect(u.Full)
 }
 
 // URLCreate POST "/" given {full} generate a slug and save it to the database then return it
 func URLCreate(c *fiber.Ctx) error {
 	c.Accepts("application/json")
 
-	var url models.URL
+	var u models.URL
 
-	if err := c.BodyParser(&url); err != nil {
+	if err := c.BodyParser(&u); err != nil {
 		return err
 	}
 
-	if matched, err := regexp.MatchString(`^(https?)://[^\s/$.?#].[^\s]*$`, url.Full); err != nil ||
-		!matched {
+	_, err := url.ParseRequestURI(u.Full)
+	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "failed to parse the url")
 	}
 
-	url.Slug = gouid.String(6, gouid.MixedCaseAlphaNum)
+	u.Slug = gouid.String(6, gouid.MixedCaseAlphaNum)
 
-	res := initializers.DB.Create(&url)
+	res := initializers.DB.Create(&u)
 	if res.Error != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "failed to generate a new entry in the db")
 	}
 
-	return c.JSON(url)
+	return c.JSON(u)
 }
